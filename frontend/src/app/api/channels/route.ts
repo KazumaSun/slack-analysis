81e7bfbf890f6acc117bda261e97c9cd043da15c
KazumaSun <satoma.2001.0920@gmail.com>
2025-04-05 16:07:27 +0000

    不要なコードの削除

--- frontend/src/app/api/channels/route.ts
+++
@@ -1,8 +0,0 @@
-// src/app/api/channels/route.ts
-
-import { NextResponse } from "next/server"
-import { channels } from "../_data"
-
-export async function GET() {
-  return NextResponse.json(channels)
-}
