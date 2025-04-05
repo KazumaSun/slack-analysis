a3f1b39e24c6dffde6f959d1083702bd5f0e4be0
uchida189 <perukipoo@icloud.com>
2025-04-05 12:56:27 +0900

    [feat]modelを追加

--- backend/repository/models.go
+++ backend/repository/models.go
@@ -1,6 +1,10 @@
 // backend/repository/models.go
 package repository
 
+import (
+	"time"
+)
+
 type User struct {
 	ID       int    `json:"id" db:"id"`
 	UserKey  string `json:"user_key" db:"user_key"`
@@ -15,6 +19,15 @@ type Team struct {
 	ChannelName string `json:"channel_name" db:"channel_name"`
 }
 
+type ActivityLog struct {
+	ID          int       `json:"id"`
+	UserID      int       `json:"user_id"`
+	TeamID      int       `json:"team_id"`
+	WorkspaceID int       `json:"workspace_id"`
+	Message     string    `json:"message"`
+	Timestamp   time.Time `json:"timestamp"`
+}
+
 type SlackUser struct {
 	ID      string `json:"id"`
 	Name    string `json:"name"`
@@ -27,4 +40,16 @@ type SlackUser struct {
 type SlackChannel struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
+}
+
+type SlackConversation struct {
+	Messages []struct {
+		User      string `json:"user"`
+		Text      string `json:"text"`
+		Timestamp string `json:"ts"`
+	} `json:"messages"`
+	HasMore          bool `json:"has_more"`
+	ResponseMetadata struct {
+		NextCursor string `json:"next_cursor"`
+	} `json:"response_metadata"`
 }
